67c50f706dd685a8a459c93ec34188bf40ecd364
Aleksandr Grigorenko <grigxia86@gmail.com>
2024-09-30 21:25:39 +0300

    Solved problem 66. Plus One

--- LeetCodeChallenge/Program.cs
+++ LeetCodeChallenge/Program.cs
@@ -14,6 +14,57 @@ namespace LeetCodeChallenge
 
     public static class Solution
     {
+        /* 66. Plus One
+         * You are given a large integer represented as an integer array digits, where each digits[i] is the ith digit of the integer. The digits are ordered from most significant to least significant in left-to-right order. The large integer does not contain any leading 0's.
+
+        Increment the large integer by one and return the resulting array of digits.
+
+        Example 1:
+
+        Input: digits = [1,2,3]
+        Output: [1,2,4]
+        Explanation: The array represents the integer 123.
+        Incrementing by one gives 123 + 1 = 124.
+        Thus, the result should be [1,2,4].
+
+        Example 3:
+
+        Input: digits = [9]
+        Output: [1,0]
+        Explanation: The array represents the integer 9.
+        Incrementing by one gives 9 + 1 = 10.
+        Thus, the result should be [1,0].
+
+         */
+        public static int[] PlusOne(int[] digits)
+        {
+            int mem = 0;
+            for (int i = digits.Length - 1; i >= 0; --i) {
+                if (mem == 1 && digits[i] + mem == 10) {
+                    digits[i] = 0;
+                }
+                else if (mem == 1) {
+                    digits[i] += 1;
+                    mem = 0;
+                    break;
+                }
+                else if (digits[i] + 1 == 10 && mem == 0) {
+                    digits[i] = 0;
+                    mem = 1;
+                }
+                else {
+                    digits[i] += 1;
+                    break;
+                }
+            }
+            if (digits[0] == 10 || mem == 1) {
+                var arr = new int[digits.Length + 1];
+                arr[0] = 1;
+                return arr;
+            }
+            return digits;
+        }
+
         /* 14. Longest Common Prefix
          * Write a function to find the longest common prefix string amongst an array of strings.
         If there is no common prefix, return an empty string "".  
