8fc1e5afc0e4cbee9897338e77f25aa62d972c3f
Aleksandr Grigorenko <grigxia86@gmail.com>
2024-10-20 17:22:58 +0300

    Solved problem 21. Merge Two Sorted Lists

--- LeetCodeChallenge/Program.cs
+++ LeetCodeChallenge/Program.cs
@@ -15,6 +15,86 @@ namespace LeetCodeChallenge
     public static class Solution
     {
 
+        /*
+        21. Merge Two Sorted Lists
+        You are given the heads of two sorted linked lists list1 and list2.
+
+        Merge the two lists into one sorted list. The list should be made by splicing together the nodes of the first two lists.
+
+        Return the head of the merged linked list.
+        Constraints:
+
+        The number of nodes in both lists is in the range [0, 50].
+        -100 <= Node.val <= 100
+        Both list1 and list2 are sorted in non-decreasing order.
+*/
+        public class ListNode
+        {
+            public int val;
+            public ListNode next;
+            public ListNode(int val = 0, ListNode next = null)
+            {
+                this.val = val;
+                this.next = next;
+            }
+        }
+
+        public static ListNode MergeTwoListsRecursive(ListNode list1, ListNode list2)
+        {
+            if (list1 == null) return list2;    
+            if (list2 == null) return list1;
+            if (list1.val <= list2.val) {
+                list1.next = MergeTwoListsRecursive(list1.next, list2);
+                return list1;
+            }
+            else {
+                list2.next = MergeTwoListsRecursive(list1, list2.next);
+                return list2;
+            }
+        }
+
+        public static ListNode MergeTwoLists(ListNode list1, ListNode list2)
+        {
+            if (list1 == null) return list2;
+            if (list2 == null) return list1;
+            ListNode headNewList = null;
+            if (list2.val > list1.val) {
+                headNewList = list1;
+                list1 = list1.next;
+            } 
+            else {
+                headNewList = list2;
+                list2 = list2.next;
+            }
+            ListNode currNode = headNewList;
+            while (list1 != null && list2 != null) {
+                if (list2.val > list1.val) {
+                    currNode.next = list1;
+                    list1 = list1.next;
+                    currNode = currNode.next;
+                }
+                else {
+                    currNode.next = list2;
+                    list2 = list2.next;
+                    currNode = currNode.next;
+                }
+            }
+            while (list1 != null) {
+                currNode.next = list1;
+                list1 = list1.next;
+                currNode = currNode.next;
+            }
+            while (list2 != null) {
+                currNode.next = list2;
+                list2 = list2.next;
+                currNode = currNode.next;
+            }
+            return headNewList;
+        }
+        // Definition for singly-linked list.
+       
+
+
 
 
         /*
