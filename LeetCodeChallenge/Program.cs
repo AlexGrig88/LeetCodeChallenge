b8aaa91002e71e4de1208fa298ecc8a475cfcd1b
Aleksandr Grigorenko <grigxia86@gmail.com>
2024-10-21 17:05:53 +0300

    Solved problem 27. Remove Element

--- LeetCodeChallenge/Program.cs
+++ LeetCodeChallenge/Program.cs
@@ -7,13 +7,56 @@ namespace LeetCodeChallenge
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("k = " + Solution.RemoveDuplicates([0, 0, 1, 1, 1, 2, 2, 3, 3, 4]));
+            Console.WriteLine("k = " + Solution.RemoveElement([0, 1, 2, 2, 3, 0, 4, 2], 2));
         }
     }
 
 
     public static class Solution
     {
+
+        /* 27. Remove Element
+         * Given an integer array nums and an integer val, remove all occurrences of val in nums in-place. 
+         * The order of the elements may be changed. Then return the number of elements in nums which are not equal to val.
+         * 
+         * Example 2:
+
+            Input: nums = [0,1,2,2,3,0,4,2], val = 2
+            Output: 5, nums = [0,1,4,0,3,_,_,_]
+            Explanation: Your function should return k = 5, with the first five elements of nums containing 0, 0, 1, 3, and 4.
+            Note that the five elements can be returned in any order.
+            It does not matter what you leave beyond the returned k (hence they are underscores).
+
+            It does not matter what you leave beyond the returned k (hence they are underscores).
+
+        Constraints:
+
+            0 <= nums.length <= 100
+            0 <= nums[i] <= 50
+            0 <= val <= 100
+         */
+        public static int RemoveElement(int[] nums, int val)
+        {
+            if (nums.Length == 0) return 0;
+            if (nums.Length == 1) return nums[0] == val ? 0 : 1;
+            int forwardIdx = 0, currIdx = 0;
+            while (currIdx < nums.Length) {
+                if (nums[currIdx] != val) {
+                    nums[forwardIdx++] = nums[currIdx];
+                    ++currIdx;
+                }
+                else {
+                    ++currIdx;
+                }
+            }
+/*            foreach (var item in nums)
+            {
+                Console.WriteLine(item);
+            }
+            Console.WriteLine("========================");*/
+            return forwardIdx;
+        }
+
         /* 26. Remove Duplicates from Sorted Array
          * Given an integer array nums sorted in non-decreasing order, remove the duplicates in-place such that each unique element appears only once. 
          * The relative order of the elements should be kept the same. Then return the number of unique elements in nums.
